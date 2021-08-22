a186952a684030bbb5bfe20e4e12ff7aa4a75c61
GiverPlay007 <play.giver.007@gmail.com>
2021-08-21 23:02:36 -0300

    Read parameters

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-maxHeap="2048"
-serverJar="server.jar"
-
 usage()
 {
   echo
@@ -13,3 +10,28 @@ usage()
   exit 1
 }
 
+while getopts "s:m:" opt
+do
+  case "$opt" in
+    s ) server="$OPTARG" ;;
+    m ) heap="$OPTARG" ;;
+    ? ) usage ;;
+  esac
+done
+
+if [ -z "$server" ]
+then
+  echo "Missing server name"
+  usage
+fi
+
+if [ -z "$heap" ]
+then
+  echo "Missing max heap size"
+  usage
+fi
+
+echo
+echo "Server: $server"
+echo "Heap: $heap"
+echo
